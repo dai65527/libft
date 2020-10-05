435a78977dc9ed955d291908e1cc4c4da99d00d3
dnakano <dnakano@student.42tokyo.jp>
2020-10-05 19:43:09 +0900

    Create: ft_strncmp.c (modify Makefile and libft.h)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 12:01:52 by dnakano           #+#    #+#             */
-/*   Updated: 2020/10/05 18:45:58 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/10/05 19:09:24 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ size_t		ft_strlcpy(char *restrict dst, const char *restrict src,
 				size_t dstsize);
 size_t		ft_strlcat(char *restrict dst, const char *restrict src,
 				size_t dstsize);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
