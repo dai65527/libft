4f6f6b2f8106948c3d231b4c61549e49c9c30044
dnakano <dnakano@student.42tokyo.jp>
2020-10-06 14:25:32 +0900

    Create: ft_strchr.c ft_strrchr.c (modify libft.h Makefile)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dnakano <dnakano@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/05 12:01:52 by dnakano           #+#    #+#             */
-/*   Updated: 2020/10/06 11:25:50 by dnakano          ###   ########.fr       */
+/*   Updated: 2020/10/06 12:43:17 by dnakano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void		*ft_memccpy(void *restrict dst, const void *restrict src,
 				int c, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
 void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
@@ -40,5 +41,7 @@ size_t		ft_strlcpy(char *restrict dst, const char *restrict src,
 size_t		ft_strlcat(char *restrict dst, const char *restrict src,
 				size_t dstsize);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
 
 #endif
